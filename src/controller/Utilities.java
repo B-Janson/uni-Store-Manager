d73b194529b43096626053b7b22ff62909698695
Brandon Janson <35671333+B-Janson@users.noreply.github.com>
2018-04-19 20:52:57 +1000

    started utilities class

---
+++ src/controller/Utilities.java
@@ -0,0 +1,13 @@
+package controller;
+
+public class Utilities {
+	
+	public static String readCSV(String fileName) {
+		return null;
+	}
+	
+	public static boolean writeCSV(String fileName, String content) {
+		return false;
+	}
+
+}
