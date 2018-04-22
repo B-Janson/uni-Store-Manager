d1047fe2a5350fe7927d37b66206ebee6ef9a9ff
Unknown <brandon.janson@smg-corporate.com>
2018-04-22 14:41:19 +1000

    wowwoweewowo

--- src/view/Main.java
+++ src/view/Main.java
@@ -3,7 +3,7 @@ package view;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("spaghetti");
+		System.out.println("spaghetti is good");
 		System.out.println("mum");
 	}
 
