52f4918312743220127ce8fbb3691f81f2fa66d6
Chris <c28.martin@connect.qut.edu.au>
2018-04-25 17:10:27 +1000

    Formatted

--- src/main/java/stock/Stock.java
+++ src/main/java/stock/Stock.java
@@ -3,42 +3,40 @@ package main.java.stock;
 import java.util.HashMap;
 import java.util.Map;
 
-import sun.reflect.generics.tree.ReturnType;
-
 public class Stock {
-	
+
 	private StockType type;
 	private Map<String, Item> itemList;
-	
-	public Stock (StockType type) {
+
+	public Stock(StockType type) {
 		this.type = type;
 		itemList = new HashMap<String, Item>();
 	}
-	
+
 	public void add(Item item) {
 		itemList.put(item.getName(), item);
 	}
-	
+
 	public void remove(Item item) {
 		itemList.remove(item.getName());
 	}
-	
+
 	public int size() {
 		return itemList.size();
 	}
-	
+
 	public StockType getStockType() {
 		return type;
 	}
-	
+
 	public void setStockType(StockType stockType) {
 		this.type = stockType;
 	}
-	
+
 	public Map<String, Item> getItemList() {
 		return itemList;
 	}
-	
+
 	public void adjustBy(Stock otherStock, boolean add) {
 		for (Item item : otherStock.getItemList().values()) {
 			if (add) {
@@ -48,7 +46,7 @@ public class Stock {
 			}
 		}
 	}
-	
+
 	public boolean contains(Item item) {
 		return itemList.containsKey(item.getName());
 	}
