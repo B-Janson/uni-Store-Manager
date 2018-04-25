ce9b721d4db21b04b5a7a2ef1aae4023342b0afb
Brandon <bjanson28@gmail.com>
2018-04-25 16:57:34 +1000

    fix up truck tests

--- src/test/java/TestTrucks.java
+++ src/test/java/TestTrucks.java
@@ -2,37 +2,77 @@ package test.java;
 
 import static org.junit.Assert.*;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Random;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import main.java.delivery.ColdTruck;
 import main.java.delivery.OrdinaryTruck;
+import main.java.delivery.Truck;
 import main.java.stock.Stock;
 import main.java.stock.StockType;
 
 public class TestTrucks {
+
+	private static final int ORDINARY_TRUCK_CAPACITY = 1000;
+	private static final int COLD_TRUCK_CAPACITY = 800;
 	
-	Stock cargo;
+	private static final double FLOATING_POINT_TOLERANCE = 0.001;
+	private static final int DECIMAL_PLACES = 2;
+	
+	private static final int TEMPERATURE_RANGE = 100;
+
+	private Random random;
+
+	private Stock cargo;
+
+	private double temperature;
 
 	@Before
 	public void setUp() throws Exception {
+		random = new Random();
 		cargo = new Stock(StockType.TruckCargo);
+
+		double temp = random.nextDouble() * random.nextInt(TEMPERATURE_RANGE) - TEMPERATURE_RANGE / 2;
+		BigDecimal bDecimal = new BigDecimal(temp);
+		bDecimal = bDecimal.setScale(DECIMAL_PLACES, RoundingMode.HALF_UP);
+
+		temperature = bDecimal.doubleValue();
+		System.out.println(temperature);
 	}
 
 	@Test
 	public void testOrdinaryTruckCost() {
-		int capacity = 1000;
-		double acceptedDelta = 0.01;
-		OrdinaryTruck testTruck = new OrdinaryTruck(capacity, cargo);
+		Truck testTruck = new OrdinaryTruck(cargo);
+		// TODO calculate expected cost of truck
 		double expected = 0.0;
-		assertEquals("cost of truck incorrect", expected, testTruck.getCost(), acceptedDelta);
+		assertEquals("cost of truck incorrect", expected, testTruck.getCost(), FLOATING_POINT_TOLERANCE);
 	}
 
 	@Test
 	public void testOrdinaryTruckCapacity() {
-		int capacity = 0;
-		OrdinaryTruck testTruck = new OrdinaryTruck(capacity, cargo);
-		int expected = 1000;
-		assertEquals("initial capacity of truck incorrect", expected, testTruck.getCapacity());
+		Truck testTruck = new OrdinaryTruck(cargo);
+		assertEquals("initial capacity of truck incorrect", ORDINARY_TRUCK_CAPACITY, testTruck.getCapacity());
+	}
+
+	@Test
+	public void testColdTruckCost() {
+		Truck testTruck = new ColdTruck(cargo, temperature);
+		int baseCost = 900;
+		int modifer = 200;
+		double tempRate = 0.7;
+		int tempScale = 5;
+		double expected = baseCost + modifer * Math.pow(tempRate, temperature / tempScale);
+		assertEquals("cost of truck incorrect", expected, testTruck.getCost(), FLOATING_POINT_TOLERANCE);
+	}
+
+	@Test
+	public void testColdTruckCapacity() {
+		Truck testTruck = new ColdTruck(cargo, temperature);
+		assertEquals("initial capacity of truck incorrect", COLD_TRUCK_CAPACITY, testTruck.getCapacity());
 	}
 
 }
