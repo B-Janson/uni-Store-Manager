74d8fdafeecd317c6183ed13ce7057df399d669f
Brandon <bjanson28@gmail.com>
2018-04-25 19:51:29 +1000

    add deliveryexception and csvException

---
+++ src/main/java/exceptions/CSVException.java
@@ -0,0 +1,34 @@
+package main.java.exceptions;
+
+public class CSVException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4498923210856881242L;
+
+	public CSVException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public CSVException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CSVException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CSVException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CSVException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/exceptions/DeliveryException.java
@@ -0,0 +1,34 @@
+package main.java.exceptions;
+
+public class DeliveryException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2461644881918330806L;
+
+	public DeliveryException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public DeliveryException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DeliveryException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DeliveryException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DeliveryException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
